8e5b8c9fce5f33008038a30728311bc45455b2a0
alif-sagir <alifsagir99@gmail.com>
2023-01-11 00:55:35 +0600

    git start

---
+++ assets/js/js1_basic_js_arithmetic_opration.js
@@ -0,0 +1,17 @@
+let a, b, addition_result, subtraction_result, multiplication_result;
+let division_result, modulus_result;
+
+a = 10;
+b = 20;
+
+addition_result = a + b;
+subtraction_result = a - b;
+multiplication_result = a * b;
+division_result = a / b;
+modulus_result = a % b;
+
+console.log("Addition : " + addition_result);
+console.log("Subtraction: " + subtraction_result);
+console.log("Multiplication : " + multiplication_result);
+console.log("Division : " + division_result);
+console.log("Modulus : " + modulus_result);
