0fbb8097af0d90fb152c8e5937f5a658702a6d6e
alif-sagir <alifsagir99@gmail.com>
2023-01-17 15:36:13 +0600

    add file

--- assets/js/js3_basic_js_function_opration.js
+++ assets/js/js3_basic_js_function_opration.js
@@ -1,8 +1,9 @@
+let deposit_tk = 0.00;
+
 function Credit_card(deposit_money=0,withdraw_money=0) {
     let calculation = ((deposit_money) - withdraw_money);
     return calculation;
 }
-let deposit_tk = 0.00;
 
 deposit_tk = Credit_card(deposit_tk,0);
 console.log("Current Amount " + deposit_tk);
